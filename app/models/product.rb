e2aa909183f355fb821b11fd4b0e174c1de9a0a4
nilpatel <nil.p@simformsolutions.com>
2023-06-02 16:47:31 +0530

    practice

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,4 @@ class Product < ApplicationRecord
   validates :product_name, length: { minimum: 3, maximum: 20 }, presence: true
   validates :price, inclusion: { in: [300..1000] }, presence: true
   validates :description, length: { minimum: 20, maximum: 60 }, presence: true
-  belongs_to :user
 end
