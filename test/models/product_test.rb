4907527864045be16f45c389ba4b474e4d07679f
nilpatel <nil.p@simformsolutions.com>
2023-06-05 10:08:23 +0530

    feat: TE7-T132 testing rails application: add tests for prducts model.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,8 +6,38 @@ class ProductTest < ActiveSupport::TestCase
     assert_not p.save, 'product instance is saving with empty attributes'
   end
 
-  test 'prodcut name should not be empty' do
+  test 'prodcut name should be present' do
     p = products(:one)
-    assert_not p.save, 'prodcut name should not be empty'
+    assert_not p.save, 'prodcut name should be present'
+  end
+
+  test 'product price should be present' do
+    p = products(:two)
+    assert_not p.save, 'product price should be present'
+  end
+
+  test 'product description should be present' do
+    p = products(:three)
+    assert_not p.save, 'product description should be present'
+  end
+
+  test 'product name should be valid.' do
+    p = products(:four)
+    assert_not p.save, 'product name should be valid.'
+  end
+
+  test 'product price should be valid' do
+    p = products(:five)
+    assert_not p.save, 'product price should be valid.'
+  end
+
+  test 'product description should be valid' do
+    p = products(:six)
+    assert_not p.save, 'product description should be valid.'
+  end
+
+  test 'product with all valid attributes should be saved.' do
+    p = products(:seven)
+    assert p.save, 'product with all valid attributes should be saved.'
   end
 end
